af42d7d5d617aaa986573c44a002c88984e886fc
hemapriya31304 <hemapriya31304@gmail.com>
2024-12-05 12:49:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-function PlayRound (HumanChoice, ComputerChoice) {
 
+function getComputerChoice() {
+    const values= ["Rock", "Paper", "Scissors"]
+    return Math.floor(Math.random() * values.length);
 }
-function getComputerChoiceInt(max) {
-    return Math.floor(Math.random() * max);
+function getHumanChoice() {
+    const choices= ["Rock", "Paper", "Scissors"]
+    if (choices = "Rock") {
+        console.log ("You chose Rock")
+    }
+    else if(choices="Paper") {
+        console.log ("You chose Paper")
+    }
+    else if (choices="Scissor") {
+        console.log("You chose Scissors")
+    }
+    else {
+        console.log("Invalid choices")
+    }
+    return choices;    
 }
-console.log(getComputerChoiceInt(3));
-function getHumanChoiceInt(max) {
-    prompt(Rock, Paper, Scissors)
-}
-console.log(Rock)
+function PlayRound (HumanChoice, ComputerChoice) {
+    if (ComputerChoice==HumanChoice)
+        console.log("Its a tie");
+    else if (ComputerChoice= "Paper")
+        if (HumanChoice= "Rock")
+        console.log ("Rock beats Paper, You Win!!");
+    else if (ComputerChoice= "Rock")
+        if (HumanChoice= "Scissors")
+        console.log ("Rock beats Scissors, You lose");
+    else if (ComputerChoice="Scissors")
+        if (HumanChoice= "Paper")
+        console.log ("Scissors beats Paper, You lose");
+    else {
+        console.log ("Your choice is invalid")
+    }
+    }
+
+
