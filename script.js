b95bd68a7c046a723bf98c7fa5c1d8fa108750b8
hemapriya31304 <hemapriya31304@gmail.com>
2024-12-05 11:29:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function PlayRound (HumanChoice, ComputerChoice) {
+
+}
+function getComputerChoiceInt(max) {
+    return Math.floor(Math.random() * max);
+}
+console.log(getComputerChoiceInt(3));
+function getHumanChoiceInt(max) {
+    prompt(Rock, Paper, Scissors)
+}
+console.log(Rock)
